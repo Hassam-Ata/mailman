e86b716a5b78b056dc88611c78c629278c0a44d4
Hassam-Ata <hassamata86@gmail.com>
2025-10-22 17:27:39 +0500

    feat(ui):Added header component

--- src/app/(workspace)/page.tsx
+++ src/app/(workspace)/page.tsx
@@ -4,8 +4,8 @@ import UserButton from "@/modules/authentication/components/user-button";
 export default async function Home() {
   const user = await currentUser();
   return (
-    <div className="flex h-screen flex-col items-center justify-center">
-      <UserButton user={user} />
+    <div className="flex h-screen flex-col items-center justify-center text-white">
+      Welcome to Pigeon
     </div>
   );
 }
